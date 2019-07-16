403e6e2335ba1e263d682ce61bb64e6a46d01f7c
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-07-16 13:21:09 -0300

    feat: adicionado avatar  do usuario e listagem de prestadores de servico

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-
-routes.post('/sessions', SessionController.store);
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.put('/users/:id', AuthMiddleware, UserController.update);
+routes.post('/sessions', SessionController.store);
+
+routes.use(AuthMiddleware);
+
+routes.put('/users/:id', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
